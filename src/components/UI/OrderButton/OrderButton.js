48a3fa179bf2cccb19d9db3a9f9f432cc671b9e2
Uday <t.uday.kanth12@gmail.com>
2022-03-27 22:05:17 +0530

    order button wiring

--- src/components/UI/OrderButton/OrderButton.js
+++ src/components/UI/OrderButton/OrderButton.js
@@ -1,12 +1,16 @@
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { pizzaActions } from '../../../store/pizzaSlice';
+import { CHECKOUT_PATH } from '../../Utils/Constants';
 import Button from '../Buttons/Button';
 import styles from './orderButton.module.css';
 
 const OrderButton = (props) => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onClickhandler = () => {
+    history.push(CHECKOUT_PATH);
     dispatch(pizzaActions.toggleIsSliced());
   };
   return (
