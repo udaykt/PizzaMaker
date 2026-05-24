471858d631e42a77cfbe420f623d606cc0c252a7
Uday <t.uday.kanth12@gmail.com>
2026-05-24 18:29:19 +0530

    refactor: convert remaining topping CSS to modules
    - Mozzarella, Olive, Pepperoni, Peppers, Sausage all moved to .module.css
    - Updated imports and className references to use styles.xxx

--- src/features/pizza/toppings/Mozzarella/Mozzarella.js
+++ src/features/pizza/toppings/Mozzarella/Mozzarella.js
@@ -1,13 +1,13 @@
 ﻿import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import './mozzarella.css';
+import styles from './mozzarella.module.css';
 
 const Mozzarella = (props) => {
   const quantity = useSelector((state) => state.pizzaHub.base.mozzarella);
 
   return (
     <Fragment>
-      {quantity.checked && <div className={'mozzarella'}></div>}
+      {quantity.checked && <div className={styles.mozzarella}></div>}
     </Fragment>
   );
 };
--- src/features/pizza/toppings/Sausage/Sausage.js
+++ src/features/pizza/toppings/Sausage/Sausage.js
@@ -1,6 +1,6 @@
 ﻿import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import './sausage.css';
+import styles from './sausage.module.css';
 
 const Sausage = (props) => {
   const quantity = useSelector((state) => state.pizzaHub.toppings.sausage);
@@ -9,12 +9,12 @@ const Sausage = (props) => {
     <Fragment>
       {quantity.checked && (
         <div>
-          <div className={'sausage'} style={props.sprinkler()}></div>
-          <div className={'sausage'} style={props.sprinkler()}></div>
+          <div className={styles.sausage} style={props.sprinkler()}></div>
+          <div className={styles.sausage} style={props.sprinkler()}></div>
         </div>
       )}
       {quantity.checked && quantity.medium && (
-        <div className={'sausage'} style={props.sprinkler()}></div>
+        <div className={styles.sausage} style={props.sprinkler()}></div>
       )}
     </Fragment>
   );
