978e7d61de57eb67af96c56afaa25384019245e4
Uday <t.uday.kanth12@gmail.com>
2026-05-24 18:29:19 +0530

    refactor: convert remaining topping CSS to modules
    - Mozzarella, Olive, Pepperoni, Peppers, Sausage all moved to .module.css
    - Updated imports and className references to use styles.xxx

--- src/features/pizza/toppings/Olive/Olive.js
+++ src/features/pizza/toppings/Olive/Olive.js
@@ -1,6 +1,6 @@
 ﻿import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import './olive.css';
+import styles from './olive.module.css';
 
 const Olive = (props) => {
   const quantity = useSelector((state) => state.pizzaHub.toppings.olives);
@@ -9,12 +9,12 @@ const Olive = (props) => {
     <Fragment>
       {quantity.checked && (
         <div>
-          <div className={'olive'} style={props.sprinkler()}></div>
-          <div className={'olive'} style={props.sprinkler()}></div>
+          <div className={styles.olive} style={props.sprinkler()}></div>
+          <div className={styles.olive} style={props.sprinkler()}></div>
         </div>
       )}
       {quantity.checked && quantity.medium && (
-        <div className={'olive'} style={props.sprinkler()}></div>
+        <div className={styles.olive} style={props.sprinkler()}></div>
       )}
     </Fragment>
   );
--- src/features/pizza/toppings/Peppers/Peppers.js
+++ src/features/pizza/toppings/Peppers/Peppers.js
@@ -1,6 +1,6 @@
 ﻿import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import './peppers.css';
+import styles from './peppers.module.css';
 
 const Peppers = (props) => {
   const quantity = useSelector((state) => state.pizzaHub.toppings.peppers);
@@ -8,12 +8,12 @@ const Peppers = (props) => {
     <Fragment>
       {quantity.checked && (
         <div>
-          <div className={`peppers`} style={props.sprinkler()}></div>
-          <div className={`peppers`} style={props.sprinkler()}></div>
+          <div className={styles.peppers} style={props.sprinkler()}></div>
+          <div className={styles.peppers} style={props.sprinkler()}></div>
         </div>
       )}
       {quantity.checked && quantity.medium && (
-        <div className={`peppers`} style={props.sprinkler()}></div>
+        <div className={styles.peppers} style={props.sprinkler()}></div>
       )}
     </Fragment>
   );
