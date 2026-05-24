1ca3437c9af620b921b31e9de872e8c2052bdf5f
Uday <t.uday.kanth12@gmail.com>
2026-05-24 18:32:51 +0530

    refactor: rename toppings folder to Toppings for naming consistency

--- src/features/pizza/Slice/Slice.js
+++ src/features/pizza/Slice/Slice.js
@@ -1,10 +1,10 @@
-﻿import Cheese from '@/features/pizza/toppings/Cheese/Cheese';
-import Mozzarella from '@/features/pizza/toppings/Mozzarella/Mozzarella';
-import Olive from '@/features/pizza/toppings/Olive/Olive';
-import Pepperoni from '@/features/pizza/toppings/Pepperoni/Pepperoni';
-import Peppers from '@/features/pizza/toppings/Peppers/Peppers';
-import Sauce from '@/features/pizza/toppings/Sauce/Sauce';
-import Sausage from '@/features/pizza/toppings/Sausage/Sausage';
+﻿import Cheese from '@/features/pizza/Toppings/Cheese/Cheese';
+import Mozzarella from '@/features/pizza/Toppings/Mozzarella/Mozzarella';
+import Olive from '@/features/pizza/Toppings/Olive/Olive';
+import Pepperoni from '@/features/pizza/Toppings/Pepperoni/Pepperoni';
+import Peppers from '@/features/pizza/Toppings/Peppers/Peppers';
+import Sauce from '@/features/pizza/Toppings/Sauce/Sauce';
+import Sausage from '@/features/pizza/Toppings/Sausage/Sausage';
 import styles from './slice.module.css';
 
 const Slice = (props) => {
