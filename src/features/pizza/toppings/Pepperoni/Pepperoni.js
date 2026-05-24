7e0a0ee07bf14200fc186d7dc687f296cccf8a3c
Uday <t.uday.kanth12@gmail.com>
2026-05-24 18:29:19 +0530

    refactor: convert remaining topping CSS to modules
    - Mozzarella, Olive, Pepperoni, Peppers, Sausage all moved to .module.css
    - Updated imports and className references to use styles.xxx

--- src/features/pizza/toppings/Pepperoni/Pepperoni.js
+++ src/features/pizza/toppings/Pepperoni/Pepperoni.js
@@ -1,6 +1,6 @@
 ﻿import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import './pepperoni.css';
+import styles from './pepperoni.module.css';
 
 const Pepperoni = (props) => {
   const quantity = useSelector((state) => state.pizzaHub.toppings.pepperoni);
@@ -9,12 +9,12 @@ const Pepperoni = (props) => {
     <Fragment>
       {quantity.checked && (
         <div>
-          <div className={'pepperoni'} style={props.sprinkler()}></div>
-          <div className={'pepperoni'} style={props.sprinkler()}></div>
+          <div className={styles.pepperoni} style={props.sprinkler()}></div>
+          <div className={styles.pepperoni} style={props.sprinkler()}></div>
         </div>
       )}
       {quantity.checked && quantity.medium && (
-        <div className={'pepperoni'} style={props.sprinkler()}></div>
+        <div className={styles.pepperoni} style={props.sprinkler()}></div>
       )}
     </Fragment>
   );
